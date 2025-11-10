1e05f49ebaeb145f85973b23f4df19d247ffb155
muhammad ahdan rafif maulana <rafifmaulana1412@gmail.com>
2025-11-10 09:04:25 +0700

    Refactor database connection for production and development

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,33 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  // Mode production (Railway)
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // biar SSL Railway bisa diterima
+      },
+    },
     logging: false,
-  }
-);
+  });
+} else {
+  // Mode development (lokal)
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: "postgres",
+      logging: false,
+    }
+  );
+}
 
 module.exports = { sequelize };
