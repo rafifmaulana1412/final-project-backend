0849d7b498812b46e695c925dc8388439fda370c
muhammad ahdan rafif maulana <rafifmaulana1412@gmail.com>
2025-11-10 12:20:12 +0700

    Refactor Cloudinary config to use URL
    Updated Cloudinary configuration to use CLOUDINARY_URL.

--- src/middlewares/uploadCloudinary_temp.js
+++ src/middlewares/uploadCloudinary_temp.js
@@ -2,20 +2,16 @@ const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("cloudinary").v2;
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+// Cloudinary otomatis baca dari process.env.CLOUDINARY_URL
+cloudinary.config();
 
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: "menus", // folder di Cloudinary tempat gambar disimpan
+    folder: "menus",
     allowed_formats: ["jpg", "jpeg", "png"],
   },
 });
 
 const upload = multer({ storage });
-
 module.exports = upload;
